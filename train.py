8f12e1da41dec43d98f9dfe90c0b57a6306549c7
w-shimaya <w.shimaya@gmail.com>
2020-10-03 13:38:42 +0900

    [add] implemented WDCGAN-gp

--- train.py
+++ train.py
@@ -3,36 +3,54 @@ import tensorflow.keras as K
 import numpy as np
 import cv2
 import os
+import argparse
 from dcgan import DCGAN
+from wdcgangp import WDCGANGP
 
 EPOCHS=200
 STEPS=2
 ZDIM=100
 
 if __name__ == "__main__":
-    dataset = K.datasets.cifar10
-    (x_train, _), (x_test, _) = dataset.load_data()
-    # upsample image 32x32 to 64x64 (no interpolation)
-    x_train = x_train.repeat(2, axis=1).repeat(2, axis=2)
-    # transform to fit to tanh
-    x_train = x_train / 128. - 1.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", choices=["cifar10", "mnist"], required=True)
+    parser.add_argument("--model", choices=["GAN", "WGAN-gp"], required=True)
+    args = parser.parse_args()
+
+    if args.dataset == "cifar10":
+        dataset = K.datasets.cifar10
+        (x_train, _), (x_test, _) = dataset.load_data()
+    elif args.dataset == "mnist":
+        dataset = K.datasets.mnist
+        (x_train, _), (x_test, _) = dataset.load_data()
+        # add color channel
+        # 1-ch -> 3-ch
+        x_train = np.concatenate([x_train[:, :, :, np.newaxis] for _ in range(3)], axis=3)
+    x_train = x_train / 128.0 - 1.
 
     train_ds = tf.data.Dataset.from_tensor_slices(x_train).shuffle(10000).batch(128)
 
-    gan = DCGAN(ZDIM)
+    if args.model == "GAN":
+        gan = DCGAN(ZDIM)
+    elif args.model == "WGAN-gp":
+        gan = WDCGANGP(ZDIM)
 
     for epoch in range(EPOCHS):
         real_batch_list = []
         accumulated_dl, accumulated_gl = 0., 0.
         for i, batch in enumerate(train_ds):
-            if len(real_batch_list) < STEPS:
-                real_batch_list.append(batch)
-                continue
-            dl, gl = gan.train_on_batch(real_batch_list)
+            scaled = tf.image.resize(batch, [64, 64])
+            if args.model == "GAN":
+                if len(real_batch_list) < STEPS:
+                    real_batch_list.append(scaled)
+                    continue
+                dl, gl = gan.train_on_batch(real_batch_list)
+                real_batch_list = []
+            elif args.model == "WGAN-gp":
+                dl, gl = gan.train_on_batch(scaled, n_critic=3)
             accumulated_dl += float(dl)
             accumulated_gl += float(gl)
-            print("[%2d] %2.4f %2.4f" % (epoch, accumulated_dl / (i + 1), accumulated_gl / (i + 1)))
-            real_batch_list = []
+            print("\r[%2d] %2.4f %2.4f" % (epoch, accumulated_dl / (i + 1), accumulated_gl / (i + 1)), end="")
 
         z = tf.random.normal((5 * 5, ZDIM))
         imgs = gan.generator(z, training=False).numpy()
@@ -42,10 +60,10 @@ if __name__ == "__main__":
             y = i % 5
             out_img[y*64:(y+1)*64, x*64:(x+1)*64, :] = im
         # insert border lines
-        out_img = np.insert(out_img, np.arange(64, 64*5, 64), np.zeros((3, )), axis=0)
-        out_img = np.insert(out_img, np.arange(64, 64*5, 64), np.zeros((3, )), axis=1)
+        out_img = np.insert(out_img, np.arange(64, 64*5, 64), -np.ones((3, )), axis=0)
+        out_img = np.insert(out_img, np.arange(64, 64*5, 64), -np.ones((3, )), axis=1)
 
-        if not os.path.exists("cifar10_images"):
-            os.makedirs("cifar10_images")
-        print(cv2.imwrite(os.path.join("cifar10_images", "epoch%d.png"%epoch), (out_img + 1.0) * 128.))
+        if not os.path.exists("%s_images"%args.dataset):
+            os.makedirs("%s_images"%args.dataset)
+        print(cv2.imwrite(os.path.join("%s_images"%args.dataset, "epoch%d.png"%epoch), (out_img[:, :, ::-1] + 1.0) * 128.))
         print("EPOCH %d finished"%epoch)
---
+++ wdcgangp.py
@@ -0,0 +1,114 @@
+import tensorflow as tf
+import tensorflow.keras as K
+import tensorflow.keras.layers as L
+
+# image size is assumed to be 32x32
+class WDCGANGP(K.Model):
+    def __init__(self, latent_dim):
+        super(WDCGANGP, self).__init__()
+        self.latent_dim = latent_dim
+
+        self.generator = K.Sequential([
+            L.Dense(2*2*1024, input_shape=(latent_dim, ), use_bias=False),
+            L.Reshape((2, 2, 1024)), 
+            L.Conv2DTranspose(512, 5, 2, padding="same", use_bias=False),
+            L.BatchNormalization(),
+            L.ReLU(), 
+            L.Conv2DTranspose(256, 5, 2, padding="same", use_bias=False),
+            L.BatchNormalization(),
+            L.ReLU(), 
+            L.Conv2DTranspose(128, 5, 2, padding="same", use_bias=False),
+            L.BatchNormalization(),
+            L.ReLU(), 
+            L.Conv2DTranspose(3, 5, 2, padding="same"),
+            L.Activation(tf.nn.tanh), 
+        ])
+
+        self.critic = K.Sequential([
+            L.Conv2D(128, 5, 2, input_shape=(32, 32, 3), padding="same"), 
+            L.LeakyReLU(0.2), 
+            L.Conv2D(256, 5, 2, padding="same", use_bias=False), 
+            #L.BatchNormalization(), 
+            L.LeakyReLU(0.2), 
+            L.Conv2D(512, 5, 2, padding="same", use_bias=False), 
+            #L.BatchNormalization(), 
+            L.LeakyReLU(0.2), 
+            L.Conv2D(1024, 5, 2, padding="same", use_bias=False), 
+            #L.BatchNormalization(), 
+            L.LeakyReLU(0.2), 
+            L.Flatten(), 
+            L.Dense(1)
+        ])
+        self.g_opt = K.optimizers.Adam(0.0002, beta_1=0.5, beta_2=0.9)
+        self.c_opt = K.optimizers.Adam(0.0002, beta_1=0.5, beta_2=0.9)
+
+    def set_weights(self, model):
+        self.generator.set_weights(model.generator.get_weights())
+        self.critic.set_weights(model.critic.get_weights())
+
+    @tf.function
+    def train_on_batch(self, real_batch_list, n_critic=5, gp_lambda=10.0):
+        batch_size = tf.shape(real_batch_list[0])[0]
+        n_critic = len(real_batch_list)
+        for real_batch in real_batch_list:
+            with tf.GradientTape() as t:
+                critic_loss, gp = self.critic_loss_fn(real_batch)
+                loss = critic_loss + gp_lambda * gp
+                #z = tf.random.normal((tf.shape(real_batch)[0], self.latent_dim))
+                #eps = tf.random.uniform((tf.shape(real_batch)[0], 1, 1, 1), minval=0., maxval=1.)
+                #fake_batch = self.generator(z, training=False)
+                #with tf.GradientTape() as tt:
+                    #blend = eps * real_batch + (1. - eps) * fake_batch
+                    #tt.watch(blend)
+                    #gp = self.critic(blend)
+                #grad = tt.gradient(gp, blend)[0]
+                #norm_grad = tf.sqrt(tf.reduce_sum(tf.square(grad), axis=[1, 2, 3]))
+
+                #critic_loss = self.critic(fake_batch) - self.critic(real_batch) + gp_lambda * tf.square(norm_grad - 1.)
+                #critic_loss = tf.reduce_mean(critic_loss)
+            critic_grad = t.gradient(loss, self.critic.trainable_variables)
+            self.c_opt.apply_gradients(zip(critic_grad, self.critic.trainable_variables))
+
+        with tf.GradientTape() as t:
+            gen_loss = self.generator_loss_fn(batch_size)
+            #z = tf.random.normal((tf.shape(real_batch)[0], self.latent_dim))
+            #fake_batch = self.generator(z)
+            #gen_loss = tf.reduce_mean(-self.critic(fake_batch, training=False))
+        gen_grad = t.gradient(gen_loss, self.generator.trainable_variables)
+        self.g_opt.apply_gradients(zip(gen_grad, self.generator.trainable_variables))
+
+        return critic_loss, gen_loss
+
+    def assign_weights(self, generator_values, critic_values):
+        for w, v in zip(self.generator.trainable_variables, generator_values):
+            w.assign(v)
+        
+        for w, v in zip(self.critic.trainable_variables, critic_values):
+            w.assign(v)
+
+    def get_weights(self):
+        generator_weights = [tf.Variable(w) for w in self.generator.trainable_variables]
+        critic_weights = [tf.Variable(w) for w in self.critic.trainable_variabels]
+
+        return generator_weights, critic_weights
+
+    def critic_loss_fn(self, real_batch):
+        z = tf.random.normal((tf.shape(real_batch)[0], self.latent_dim))
+        eps = tf.random.uniform((tf.shape(real_batch)[0], 1, 1, 1), minval=0., maxval=1.)
+        fake_batch = self.generator(z, training=False)
+        with tf.GradientTape() as tt:
+            blend = eps * real_batch + (1. - eps) * fake_batch
+            tt.watch(blend)
+            gp = self.critic(blend)
+        grad = tt.gradient(gp, blend)
+        norm_grad = tf.sqrt(tf.reduce_sum(tf.square(grad), axis=[1, 2, 3]))
+
+        critic_loss = tf.reduce_mean(self.critic(fake_batch) - self.critic(real_batch))
+        gradient_penalty = tf.reduce_mean(tf.square(norm_grad - 1.))
+        return critic_loss, gradient_penalty
+
+    def generator_loss_fn(self, batch_size):
+        z = tf.random.normal((batch_size, self.latent_dim))
+        fake_batch = self.generator(z)
+        gen_loss = tf.reduce_mean(-self.critic(fake_batch, training=False))
+        return gen_loss
